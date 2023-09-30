f46892f6774f1555bd478ae1e736ff7528bf2719
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-30 09:21:58 -0300

    feat: add drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+  path: '.env',
+})
+
+export default {
+  driver: 'pg',
+  schema: './src/lib/db/schema.ts',
+  dbCredentials: {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    connectionString: process.env.DATABASE_URL!,
+  },
+} satisfies Config
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '@clerk/nextjs'
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   // Ensure that locale-specific sign in pages are public
-  publicRoutes: ['/sign-in', '/sign-up'],
+  publicRoutes: ['/', '/sign-in', '/sign-up'],
 })
 
 export const config = {
