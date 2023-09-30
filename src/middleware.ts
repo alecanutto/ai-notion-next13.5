1d87d6bc4aaff58c3ce18f0be62119b0334378b7
Alessandra Canuto <ale.canutto@gmail.com>
2023-09-30 09:28:24 -0300

    fix: move middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,7 @@ import { authMiddleware } from '@clerk/nextjs'
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  // Ensure that locale-specific sign in pages are public
-  publicRoutes: ['/', '/sign-in', '/sign-up'],
+  publicRoutes: ['/'],
 })
 
 export const config = {
